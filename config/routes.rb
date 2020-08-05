ed401d95029339e65169c8e21c66a32dca3fd492
noranuko13 <noranuko13@gmail.com>
2020-08-05 11:01:54 +0900

    :docs: ルーティングに関するドキュメントへのリンク位置の移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+# Rails Routing from the Outside In
+# https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root 'pages#index'
 
   get '/about', to: 'pages#about'
