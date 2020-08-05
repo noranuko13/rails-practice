22e954503d16a970eb903d084ac123919227ab82
noranuko13 <noranuko13@gmail.com>
2020-08-05 13:03:50 +0900

    :zap: rubocop --auto-correct

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,8 +19,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Allow overriding of the sprockets cache path
 Rails.application.config.assets.configure do |env|
   env.cache = Sprockets::Cache::FileStore.new(
-      ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
-      Rails.application.config.assets.cache_limit,
-      env.logger
+    ENV.fetch('SPROCKETS_CACHE', "#{env.root}/tmp/cache/assets"),
+    Rails.application.config.assets.cache_limit,
+    env.logger
   )
 end
