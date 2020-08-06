bbb0a073a7028f76ee1e74b1a7f61f2871274b4f
noranuko13 <noranuko13@gmail.com>
2020-08-07 02:53:00 +0900

    :notes: ライブラリに依存するヘルパーはプロジェクトのヘルパーを経由

---
+++ app/helpers/rp_form_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RpFormHelper
+  def rp_form_with(options = {}, &block)
+    form_with(**options, &block)
+  end
+
+  def rp_search_form_for(record, **options, &block)
+    search_form_for(record, options, &block)
+  end
+
+  # FIXME: What about to add the helper search_form_with ?
+  # https://github.com/activerecord-hackery/ransack/issues/962
+end
