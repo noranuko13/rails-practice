baa32f205493a5ad7a27181954edb3ad24009c75
noranuko13 <noranuko13@gmail.com>
2020-08-07 21:39:47 +0900

    :zap: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_015757) do
+ActiveRecord::Schema.define(version: 2020_08_07_123324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.decimal "price", precision: 10, scale: 2
+    t.decimal "regular_price", precision: 10, scale: 2
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
